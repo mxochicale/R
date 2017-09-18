48fa4b6a5b4a175f2aac26f4774671e9df23d06d
mxochicale <perez.xochicale@gmail.com>
2017-09-18 19:05:53 +0100

    rpackages/nonlinearTseries/

--- rpackages/nonlinearTseries/R/mutualInformation.R
+++ rpackages/nonlinearTseries/R/mutualInformation.R
@@ -2,64 +2,63 @@
 #' @description
 #' Functions for estimating the Average Mutual Information (AMI) of a time
 #' series.
-#' @details 
+#' @details
 #' The Average Mutual Information (AMI) measures how much one random variable
 #'  tells us about another. In the context of time series analysis, AMI
 #' helps to quantify the amount of knowledge gained about the value
 #' of \eqn{x(t+\tau)}{x(t+tau)} when observing \eqn{x(t)}.
-#' 
-#' To measure the AMI iof a time series, we create a histogram of the data
-#' using bins. Let \eqn{p_i}{Pi} the probability that the signal has a 
+#'
+#' To measure the AMI of a time series, we create a histogram of the data
+#' using bins. Let \eqn{p_i}{Pi} the probability that the signal has a
 #' value inside the ith bin, and let  \eqn{p_{ij}(\tau)}{Pij(tau)} be
-#' the probability that \eqn{x(t)} is in bin i ans \eqn{x(t+\tau)}{x(t+tau)}
+#' the probability that \eqn{x(t)} is in bin i and \eqn{x(t+\tau)}{x(t+tau)}
 #' is in bin j. Then, AMI for time delay \eqn{\tau}{tau} is defined as
-#' 
+#'
 #' \deqn{AMI(\tau) = \sum_{i,j} p_{ij} log(\frac{p_{ij}}{p_i p_j})}{
 #' AMI(tau) = sum( Pij log( Pij / (Pi*Pj) ) ) }
-#' 
-#' Depending on the base of the logarithm used to define AMI, the AMI
-#' is measured in bits (base 2, also called shannons), nats (base e) or
-#'  bans (base 10, also called hartleys).
+#'
+#' The AMI can either be measured in bits (base 2, also called shannons),
+#' nats (base e) or bans (base 10, also called hartleys).
 #' @param time.series The observed time series.
 #' @param lag.max Largest lag at which to calculate the AMI.
-#' @param n.partitions Number of bins used to compute the probability 
+#' @param n.partitions Number of bins used to compute the probability
 #' distribution of the time series.
 #' @param units The units for the mutual information. Allowed units are
 #' "Nats", "Bits" or "Bans" (somethings called Hartleys). Default is "Nats".
 #' @param do.plot Logical value. If TRUE, the AMI is plotted
 #' @param ... Further arguments for the plotting function.
-#' @return  A \emph{mutualInf} object that consist of a list containing all 
-#' the relevant information of the AMI computation: 
-#' \emph{time.lag}, \emph{mutual.information}, \emph{units} and 
-#' \emph{n.partitions}. 
-#' @references H. Kantz  and T. Schreiber: Nonlinear Time series Analysis 
+#' @return  A \emph{mutualInf} object that consist of a list containing all
+#' the relevant information of the AMI computation:
+#' \emph{time.lag}, \emph{mutual.information}, \emph{units} and
+#' \emph{n.partitions}.
+#' @references H. Kantz  and T. Schreiber: Nonlinear Time series Analysis
 #' (Cambridge university press)
 #' H. Abarbanel: Analysis of observed chaotic data (Springer, 1996).
 #' @author Constantino A. Garcia
-#' @examples 
+#' @examples
 #' \dontrun{
 #' sx = sinaiMap(a=0.3,n.sample=5000,start=c(0.23489,0.8923),do.plot=FALSE)$x
 #' mutinf = mutualInformation(sx, n.partitions = 20, units = "Bits") }
 #' @seealso \code{\link{timeLag}}
-#' @rdname mutualInformation 
+#' @rdname mutualInformation
 #' @export mutualInformation
 #' @exportClass mutualInf
 #' @useDynLib nonlinearTseries
 #' @import Rcpp
-mutualInformation = function(time.series,lag.max = NULL, 
-                              n.partitions = NULL, 
+mutualInformation = function(time.series,lag.max = NULL,
+                              n.partitions = NULL,
                               units = c("Nats","Bits","Bans"),
                               do.plot=TRUE,
                               ...){
   units = match.arg(units)
-  base = switch(units, 
+  base = switch(units,
                 Nats = exp(1),
                 Bits = 2,
                 Bans = 10)
-  
+
   if (is.null(lag.max)) {
     lag.max = max(20, sqrt(length(time.series)))
-  }  
+  }
   if (is.null(n.partitions)) {
     n.partitions = max(floor(length(time.series) ^ (1 / 3)), 2)
   }
@@ -69,8 +68,8 @@ mutualInformation = function(time.series,lag.max = NULL,
                  as.integer(n.partitions),
                  PACKAGE = "nonlinearTseries" )
   mutinf = mutualInf(0:lag.max, mutinf / log(base), units, n.partitions)
-  
-  if (do.plot) { 
+
+  if (do.plot) {
     tryCatch(plot(mutinf), error = function(error){
       warning("Error while trying to plot the mutual informaiton")
     })
@@ -92,7 +91,7 @@ plot.mutualInf = function(x,main="Average Mutual Information (AMI)",
     ylab = paste("AMI in", x$units)
   }
   x.len = length(x$mutual.information)
-  plot(x$time.lag, x$mutual.information, type = type, main = main, 
+  plot(x$time.lag, x$mutual.information, type = type, main = main,
        xlab = xlab, ylab = ylab, ...)
 }
 
