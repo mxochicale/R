efa2add3b54254802b80b03120b0e22afe9afb07
Miguel Xochicale <perez.xochicale@gmail.com>
2018-03-05 01:43:03 +0000

    upgradeR/install-basic-packages.R

--- upgradeR/install-basic-packages.R
+++ upgradeR/install-basic-packages.R
@@ -1,5 +1,4 @@
 
-
 mirror_repo <- 'https://www.stats.bris.ac.uk/R/'
 
 ## OTHER UK MIRRORS
@@ -13,24 +12,22 @@ mirror_repo <- 'https://www.stats.bris.ac.uk/R/'
 
 
 
-
-
-
 ### devtools
 install.packages("devtools", repos=mirror_repo, dependencies = TRUE)
-
 
 #### [data.table]([https://github.com/Rdatatable/data.table/wiki/Installation)
 #[MORE](https://github.com/Rdatatable/data.table/wiki/Installation#why-we-recommend-installpackages-above-rather-than-devtoolsinstall_github)
 library(devtools)
 install_github("Rdatatable/data.table", build_vignettes=FALSE)
 
-
 ### ggplot2
 install.packages("ggplot2", repos=mirror_repo, dependencies = TRUE)
 
 ####  for savitzly-golay filter
 install.packages("signal", repos=mirror_repo, dependencies = TRUE)
+
+####  for deSolve
+install.packages("deSolve", repos=mirror_repo, dependencies = TRUE)
 
 ### plot3D
 .packages <- c('car', 'scatterplot3d', 'plot3D')
